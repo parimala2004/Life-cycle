d792114991f1be5edce0501fa025985560a4d2a5
parimala2004 <145036239+parimala2004@users.noreply.github.com>
2025-05-24 21:24:29 +0530

    Update cycle.dart

--- lib/cycle.dart
+++ lib/cycle.dart
@@ -1,253 +1,270 @@
 import 'package:flutter/material.dart';
-class LifeCyclePage extends StatefulWidget {
-  final String title;
 
-  LifeCyclePage({Key? key, required this.title}) : super(key: key);
+class form extends StatefulWidget {
+  const form({super.key});
 
   @override
-  _LifeCyclePageState createState() {
-    print('createState triggered');
-    return _LifeCyclePageState();
-  }
+  State<form> createState() => _formState();
 }
 
-class _LifeCyclePageState extends State<LifeCyclePage> with WidgetsBindingObserver,SingleTickerProviderStateMixin {
-  int _counter = 0;
-  int _selectedIndex = 0;
-  String _appState = "App started";
-  late TabController _tabController;
-  String _gender = 'Male';
-
+class _formState extends State<form> with SingleTickerProviderStateMixin{
+  final _formKey= GlobalKey<FormState>();
+  final TextEditingController Controller = TextEditingController();
+  final TextEditingController _formFieldController = TextEditingController();
+  final TextEditingController _formfieldController = TextEditingController();
+  final TextEditingController _formfieldcontroller = TextEditingController();
+  final TextEditingController _formFieldcontroller = TextEditingController();
+  String? _selectedCity;
+  String? _selectedDepartment;
+  List<String> _cities = ['Chennai', 'Bangalore', 'Hyderabad', 'Mumbai','Coimbatore','gujarat','Rajastan','Punjab','Delhi','Kolkata','Lucknow'];
+  List<String> _departments = ['CSE', 'ECE', 'EEE', 'MECH'];
+  late TabController tabController;
   @override
-  void initState() {
+  void initState(){
     super.initState();
-    _tabController = TabController(length: 4, vsync: this);
-    print('initState called');
-    WidgetsBinding.instance.addObserver(this);
-  }
+    tabController=TabController(length: 2, vsync: this);
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('didChangeDependencies called');
   }
-
-  @override
-  void didUpdateWidget(covariant LifeCyclePage oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print('didUpdateWidget called');
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    setState(() {
-      switch (state) {
-        case AppLifecycleState.resumed:
-          print('app resumed');
-          break;
-        case AppLifecycleState.paused:
-          print('app paused');
-          break;
-        case AppLifecycleState.inactive:
-          print('app inactive');
-          break;
-        case AppLifecycleState.detached:
-        default:
-          print('app detached');
-          break;
-      }
-    });
-    print('AppLifecycleState changed to: $state');
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('Counter increased to $_counter'),
-      duration: Duration(seconds: 1),
-    ));
-  }
-
-  void _onNavTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    print('build method called');
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.blueGrey,
-        bottom: TabBar(
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicatorAnimation: TabIndicatorAnimation.elastic,
-          controller: _tabController,
-          tabs: [
-            Tab(text: "Home"),
-            Tab(text: "Orders"),
-            Tab(text: "Favourite"),
-            Tab(text: "Profile"),
-          ],
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.indigo),
-              child: Text("MENU", style: TextStyle(color: Colors.white, fontSize: 20)),
-            ),
-            ListTile(
-              title: Text("Click here"),
-              onTap: () {
-                Navigator.pop(context);
-                showDialog(
-                  context: context,
-                  builder: (ctx) => AlertDialog(
-                    title: Text("Here is a Alert Box"),
-                    content: Text("Hello foodie"),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(ctx),
-                        child: Text("OK"),
-                      )
-                    ],
-                  ),
-                );},
-            )],
-        ),),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-         Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Lifecycle Status: $_appState", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
-                SizedBox(height: 10),
-                Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(20),
-                  child: Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Column(
-                      children: [
-                        Text("You clicked $_counter times.", style: TextStyle(fontSize: 18)),
-                        SizedBox(height: 12),
-                        Image.asset("assets/images/fast food.jpg", height: 180, fit: BoxFit.cover),
-                        SizedBox(height: 12),
-                        ElevatedButton(
-                          onPressed: _incrementCounter,
-                          child: Text("Click here",style: TextStyle(color: Colors.blueGrey),),
+        appBar: AppBar(
+          title: Text('Welcome',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+          backgroundColor: Colors.blue,
+          bottom: TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorAnimation: TabIndicatorAnimation.elastic,
+              controller: tabController,
+              tabs:[Tab(text:' Order Form'),
+                Tab(text:'Favourites'),
 
+              ] ),
+        ),
+        body:TabBarView(
+            controller: tabController,
+            children: [SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Name:", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        TextField(
+                          controller: Controller,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter name',
+                            labelText: 'Enter name',
+                            suffixIcon:Icon(Icons.person),
+                            filled: true,
+                            fillColor: Colors.white12,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text("Address:", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        TextFormField(
+                          controller: _formFieldController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter  the address',
+                            suffixIcon: Icon(Icons.home_outlined),
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'address is required';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text("Phone number:", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        TextFormField(
+                          controller: _formfieldController,
+                          keyboardType: TextInputType.datetime,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter  the number',
+                            suffixIcon: Icon(Icons.phone),
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'number is required';
+                            }
+                            else if (! RegExp(r'^\d{10}$').hasMatch(value)){
+                              return 'enter a valid number';
+
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text("Date Of Birth", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        TextFormField(
+                          controller: _formFieldcontroller,
+                          keyboardType: TextInputType.datetime,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'yyyy-mm-dd',
+                            hintText: 'DOB',
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'DOB is required';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text("Email", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        TextFormField(
+                          controller: _formfieldcontroller,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your mail',
+                            suffixIcon: Icon(Icons.email_outlined),
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                          validator: (value)
+                          {
+                            final pattern= RegExp(r'^[a-zA-Z0-9.a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}');
+                            if(value!.isEmpty)
+                            {
+                              return 'required';
+                            }
+                            else if(pattern.hasMatch(value))
+                            {
+                              return null;
+                            }
+                            else
+                            {
+                              return "enter a valid email";
+                            }
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text("City:", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        DropdownButtonFormField<String>(
+                          value: _selectedCity,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                          hint: Text('Select a city'),
+                          items: _cities.map((city) {
+                            return DropdownMenuItem(
+                              value: city,
+                              child: Text(city),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedCity = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please select a city';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text("Department", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.blue,fontSize:20),),
+                        SizedBox(height: 5),
+                        DropdownButtonFormField<String>(
+                          value: _selectedDepartment,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.grey.shade300,
+                          ),
+                          hint: Text('Select a department'),
+                          items: _departments.map((dept) {
+                            return DropdownMenuItem(
+                              value: dept,
+                              child: Text(dept),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedDepartment = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null) {
+                              return 'select a department';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 30),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                (      SnackBar(content: Text('Form submitted successfully')),
+                                );
+                              }
+                            },
+                            child: Text('Submit',style: TextStyle(color: Colors.blueAccent),),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-          ),
-        ),
-        Card(
-        color: Colors.greenAccent.shade100,
-        margin: EdgeInsets.all(16),
-        elevation: 5,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-
-        children: [
-
-
-        ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: Image.asset(
-        'assets/images/fast food.jpg',
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
-        ),
-        ),
-    SizedBox(height: 16),
-
-    Text(
-    "Pizza Order Details",
-    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-    ),
-    SizedBox(height: 12),
-
-
-    Align(
-    alignment: Alignment.centerLeft,
-    child: Text("Select Gender:", style: TextStyle(fontWeight: FontWeight.bold)),
-    ),
-    Row(
-    children: [
-    Radio<String>(
-    value: "Male",
-    groupValue: _gender,
-    onChanged: (val) {
-    setState(() {
-    _gender = val!;
-    });
-    },
-    ),
-    Text("Male"),
-    Radio<String>(
-    value: "Female",
-    groupValue: _gender,
-    onChanged: (val) {
-    setState(() {
-    _gender = val!;
-    });
-    },
-    ),
-    Text("Female"),
-    ],
-    ),
-    ],),),),
-         CircleAvatar(
-           radius: 6,
-           child: Text("Hello Foodie",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
-           backgroundColor: Colors.brown,
-         ),
-          Center(child: Text('Hello'),)
-          
-
-        ]
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onNavTapped,
-        selectedItemColor: Colors.indigo,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.info), label: "Info"),
-        ],
-      ),
+              SecondScreen(),
+            ]
+        )
     );
   }
-
+}
+class SecondScreen extends StatelessWidget {
   @override
-  void deactivate() {
-    super.deactivate();
-    print('deactivate called');
-  }
+  Widget build(BuildContext context) {
+    return Scaffold(
 
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    print('dispose called');
-    super.dispose();
+        body:Card(
+          color: Colors.greenAccent.shade100,
+          margin: EdgeInsets.all(50),
+          elevation: 9,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+
+
+          child: Column(
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/fast food.jpg', height: 200, width: 500, fit: BoxFit.cover,)
+              ),
+              Text("order Desert")
+            ],
+          ),
+
+
+
+
+        )
+    );
+
   }
 }
